b6d2639173feb323ebcba74b5da110ccc1039302
Dametorwobla <dametorrwobla@gmail.com>
2022-09-01 22:44:51 -0700

    roman numeral to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
 
-
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
+    if not roman_string:
+        return 0
     if (not isinstance(roman_string, str) or
             roman_string is None):
-        return (0)
+        return 0
 
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
+    sum_n = 0
+    roman_n = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
     }
-    num = 0
 
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
+    len_n = len(roman_string)
+    for x in range(len_n):
+        if roman_n.get(roman_string[x], 0) == 0:
+            return 0
 
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-
+        if (x != (len_n - 1) and
+                roman_n[roman_string[x]] < roman_n[roman_string[x + 1]]):
+            sum_n += roman_n[roman_string[x]] * -1
         else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+            sum_n += roman_n[roman_string[x]]
+    return sum_n
