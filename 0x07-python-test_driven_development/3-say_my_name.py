a1ef29a685fbe6735ee002de9462d31b82ee08f8
Dametorwobla <dametorwobla@gmsil.com>
2022-10-01 20:24:37 -0700

    test driven

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-
-"""
-Prints a string that include the names passed
-"""
+"""Defines a name-printing function."""
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints the format My name is <first name> <last name>
+    """Print a name.
     Args:
-        first_name (string): first name
-        last_name (string): last name
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
     """
-
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
